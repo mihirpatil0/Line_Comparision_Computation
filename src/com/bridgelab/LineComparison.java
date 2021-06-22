89e6f6fe872bfa791eef03221eb1b6c83d9e5b23
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 18:49:22 +0530

    Added: LineComparison java file which is printing welcome message.

---
+++ src/com/bridgelab/LineComparison.java
@@ -0,0 +1,9 @@
+package com.bridgelab;
+
+public class LineComparison
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to line comparision computation problem.");
+	}
+}
