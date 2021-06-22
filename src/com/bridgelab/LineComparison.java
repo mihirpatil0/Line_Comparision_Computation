9e467fc3af1c9e52e41434eda2944d0e686918cc
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-22 19:04:50 +0530

    Modified: Comparing two lines length for that taking input from user and then using equals() method to compare to lines and printing the output.

--- src/com/bridgelab/LineComparison.java
+++ src/com/bridgelab/LineComparison.java
@@ -8,7 +8,7 @@ public class LineComparison
 	
 	public static void main(String[] args)
 	{
-		float x1=0, x2=0, y1=0, y2=0;
+		float x1=0, x2=0, y1=0, y2=0, a1=0, a2=0, b1=0, b2=0;
 		
 		System.out.print("Enter the value of a point x1: "); 
 		x1 = scanner.nextFloat();
@@ -18,9 +18,30 @@ public class LineComparison
 		x2 = scanner.nextFloat();
 		System.out.print("Enter the value of a point y2: "); 
 		y2 = scanner.nextFloat();
+		System.out.print("Enter the value of a point a1: "); 
+		a1 = scanner.nextFloat();
+		System.out.print("Enter the value of a point b1: "); 
+		b1 = scanner.nextFloat();
+		System.out.print("Enter the value of a point a2: "); 
+		a2 = scanner.nextFloat();
+		System.out.print("Enter the value of a point b2: "); 
+		b2 = scanner.nextFloat();
 		
-		//Calculating length of an line using math.sqrt() function.
-		float length =(float) Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
-		System.out.println("\nThe length of a line is:" + length);
+		//Calculating length of lines using math.sqrt() function that is accepting number from user.
+		float firstLineLength =(float) Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
+		float secondLineLength =(float) Math.sqrt( ( ( a2 - a1 ) * ( a2 - a1 ) ) + ( ( b2 - b1 ) * ( b2 - b1 ) ) );
+		
+		System.out.println("\nThe length of a first line is:" + firstLineLength);
+		System.out.println("The length of a second line is:" + secondLineLength);
+		Float obj1 = firstLineLength;
+		Float obj2 = secondLineLength;
+		if (obj1.equals(obj2))
+		{
+			System.out.println("Both the line are equal");
+		}
+		else
+		{
+			System.out.println("Lines are not equal");
+		}
 	}
 }
